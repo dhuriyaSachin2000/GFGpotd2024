ebef919990f438d9b72d8bad9cf656e9ba232a67
Sachin Kumar Dhuriya <110597225+dhuriyaSachin2000@users.noreply.github.com>
2024-01-15 09:30:48 +0530

    Find duplicate rows in a binary matrix

---
+++ 14jan2024.cpp
@@ -0,0 +1,26 @@
+class Solution
+{   
+    public:
+    vector<int> repeatedRows(vector<vector<int>> &matrix, int M, int N) 
+    { 
+        vector<int> res;
+        unordered_set<int> set;
+        int r=M;
+        int c=N;
+        
+        for(int i=0;i<r;i++){
+            int indx=0;
+            for(int j=0;j<c;j++){
+                 indx += matrix[i][j] * pow(2, c-1-j);
+            }
+             if (set.find(indx) != set.end()) {
+              res.push_back(i);
+        }
+        else {
+            set.insert(indx);
+        }
+        }
+        return res;
+      
+    } 
+};
